9f149a18a866ec72fc405752b306e544188b4189
Jesse Nauman <jnauman619@gmail.com>
2021-08-04 21:10:43 -0500

    Make sign up page work.

--- console_inventory/authentication/routes.py
+++ console_inventory/authentication/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from console_inventory.forms import UserLoginForm
+from console_inventory.models import User, db
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
@@ -10,8 +11,22 @@ def signup():
         email = form.email.data
         password = form.password.data
         print(email, password)
+
+        user = User(email, password)
+        db.session.add(user)
+        db.session.commit()
+
+        flash(f'Your account has been created: {email}', 'user-created')
+        return redirect(url_for('site.home'))
+        
     return render_template('signup.html', form = form)
 
-@auth.route('/signin')
+@auth.route('/signin', methods = ['GET', 'POST'])
 def signin():
-    return render_template('signin.html')
+    form = UserLoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        print(email, password)
+
+    return render_template('signin.html', form = form)
