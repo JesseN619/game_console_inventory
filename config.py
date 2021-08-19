7d58c518d93f888d5b00ede6db45a9b8a1f5752a
Jesse Nauman <jnauman619@gmail.com>
2021-08-18 20:09:34 -0500

    Prepare for Heroku deployment

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY= "12345"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
